702df6aca1c5248d75f165119e58e1adf7acdf90
russ <russvetsper@gmail.com>
2016-10-19 14:48:15 -0700

    add  bootstrap(initialize the app)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1>My First Angular 2 App</h1>
+  </div>
   `
 })
 
